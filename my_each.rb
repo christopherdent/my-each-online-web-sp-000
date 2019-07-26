3374133b2ff245d7e68ccbd15af7dfcda444a16c
Christopher Dent <christopherdent01@gmail.com>
2019-07-26 01:25:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,14 @@
+array = ["Joe", "Sam", "Ghandi", "Mojo"]
+
 def my_each(array)
    while i < array.length 
       yield (array[i])
       i = i + 1 
     end 
-    
   end 
+  
+  
+  
   
   my_each(array) do |item|
     if item.length(3)
